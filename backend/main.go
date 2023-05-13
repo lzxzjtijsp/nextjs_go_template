095af7cbb91cc94d579a70f0250688d4294fca58
lzxzjtijsp <shirok420@gmail.com>
2023-05-13 18:46:12 +0900

    Gin and Next.js appDir Init

--- backend/main.go
+++ backend/main.go
@@ -3,17 +3,37 @@ package main
 import (
 	"backend/app/middleware"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"path"
+	"path/filepath"
 )
 
-import "net/http"
-
 func main() {
-	engine := gin.Default()
-	engine.Use(middleware.Cors())
-	engine.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "hello world",
+	router := gin.Default()
+	router.Use(middleware.Cors())
+
+	api := router.Group("/api") // APIのルートを/apiに変更
+	{
+		api.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "hello world",
+			})
 		})
+	}
+
+	router.NoRoute(func(c *gin.Context) { // 未知のルートに対するハンドラ
+		dir, file := path.Split(c.Request.RequestURI)
+		extension := filepath.Ext(file)
+
+		// リクエストがファイルであることを確認する (".html", ".css", etc.)
+		// リクエストがディレクトリの場合は "index.html"
+		if file == "" || extension == "" {
+			path := filepath.Join(dir, "index.html")
+			c.File("../frontend/out" + path)
+		} else {
+			c.File("../frontend/out" + c.Request.RequestURI)
+		}
 	})
-	engine.Run(":8080")
+
+	router.Run(":8080")
 }
