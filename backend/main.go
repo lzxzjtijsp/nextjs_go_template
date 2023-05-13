d4061e36866b05c813c2bb3985c831cc3ec0b502
lzxzjtijsp <shirok420@gmail.com>
2023-05-13 18:13:02 +0900

    Gin and Next.js appDir Init

---
+++ backend/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"backend/app/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+import "net/http"
+
+func main() {
+	engine := gin.Default()
+	engine.Use(middleware.Cors())
+	engine.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello world",
+		})
+	})
+	engine.Run(":8080")
+}
